103f8b47073572768bd24e3d3a14c99b37906cc9
pacificyost <pacificyost@gmail.com>
2018-04-21 16:09:39 -0700

    A different game concept
    A different game concept

---
+++ LD41 2nd try/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    private float settledThreshold = 0.01f;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool Settled()
+    {
+        return Mathf.Abs(rb.velocity.magnitude) < settledThreshold;
+        
+    }
+}
---
+++ LD41 2nd try/Assets/Scripts/Trap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour {
+
+    PlayerController pc;
+    BoxCollider boxCollider;
+
+	// Use this for initialization
+	void Start () {
+        pc = GameObject.FindObjectOfType<PlayerController>();
+        boxCollider = GetComponent<BoxCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Entered");
+        if (pc.Settled())
+        {
+            Debug.Log("Settled");
+            boxCollider.enabled = false;
+        }
+    }
+}
